2f8bc5f2675e49d3e580548460d6a9f405d46020
arthur <87774958+Arthur-T-Tavares@users.noreply.github.com>
2021-09-23 15:31:45 -0300

    pagina informe cpf

--- lib/Routes/app_pages.dart
+++ lib/Routes/app_pages.dart
@@ -3,12 +3,13 @@ import 'package:neo/Routes/app_routes.dart';
 import 'package:neo/Screen/cotacao/cadastro.dart';
 import 'package:neo/Screen/cotacao/informe_cep_faculdade.dart';
 import 'package:neo/Screen/cotacao/informe_cep_trabalho.dart';
+import 'package:neo/Screen/cotacao/informe_cpf.dart';
 import 'package:neo/Screen/cotacao/opcoes_uso_veiculo.dart';
 import 'package:neo/Screen/splash.dart';
 // ignore: import_of_legacy_library_into_null_safe
 
 class AppPages {
   static final routes = [
-    GetPage(name: Routes.INITIAL, page: () => InformeCEPFaculdade())
+    GetPage(name: Routes.INITIAL, page: () => InformeCPF())
   ];
 }
---
+++ lib/Screen/cotacao/informe_cpf.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:neo/Screen/componentes_globais/componentes_temas/cores.dart';
+import 'package:neo/Screen/componentes_globais/inputs/input_texto.dart';
+import 'package:neo/Screen/componentes_globais/scaffold_principal.dart';
+import 'package:neo/Screen/componentes_globais/textos_header_componentes/header.dart';
+
+class InformeCPF extends StatefulWidget {
+  const InformeCPF({Key? key}) : super(key: key);
+
+  @override
+  _InformeCPFState createState() => _InformeCPFState();
+}
+
+class _InformeCPFState extends State<InformeCPF> {
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    var size = mediaQuery.size;
+    return ScaffoldPrincipal(conteudo: _corpo(size));
+  }
+
+  Widget _corpo(Size size) {
+    return Container(
+      width: size.width,
+      height: size.height,
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildListDelegate(List.generate(
+              1,
+              (i) => Column(
+                children: [
+                  Column(
+                    children: [
+                      _campoCEP(size),
+                    ],
+                  )
+                ],
+              ),
+            ).toList()),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _campoCEP(Size size) {
+    return Column(children: [
+      MainHeader(
+          paddingLados: size.width * 0.2,
+          size: size,
+          text: 'Me informa o seu CPF'),
+      Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: size.width * 0.05, vertical: size.height * 0.01),
+        child: Container(
+            color: AppCores.BRANCO,
+            child: Column(
+              children: [
+                SizedBox(height: size.height * 0.01),
+                InputTexto(size: size, text: 'CPF', h: 0.01, v: 0.05),
+                SizedBox(height: size.height * 0.01),
+              ],
+            )),
+      )
+    ]);
+  }
+}
