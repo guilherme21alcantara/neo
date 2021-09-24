0f78a05b56031523b1d3197a066e6924465840dc
guilherme21alcantara <guilhermeafonsoalcantara@gmail.com>
2021-09-24 11:18:28 -0300

    tela buscar

---
+++ lib/Screen/cotacao/buscar_placa.dart
@@ -0,0 +1,18 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:neo/Screen/componentes_globais/scaffold_principal.dart';
+import 'package:neo/Screen/texto_globais/textos_gerais/titulos/titulos.dart';
+import 'package:neo/widgets/widgets_lista_cotacao.dart';
+
+class BuscarPlaca extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaQuery = MediaQuery.of(context);
+    var size = mediaQuery.size;
+    return ScaffoldPrincipal(
+      title: TITULOS.COTACAO,
+      conteudo: lista(size),
+    );
+  }
+}
