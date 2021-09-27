c24fab5a8d5b7619589f90e4dd4d558d100b42b4
arthur <87774958+Arthur-T-Tavares@users.noreply.github.com>
2021-09-27 18:44:45 -0300

    limpeza de código

--- lib/Screen/conta/esqueceu_senha.dart
+++ lib/Screen/conta/esqueceu_senha.dart
@@ -37,66 +37,75 @@ class _EsqueceuSenhaPageState extends State<EsqueceuSenhaPage> {
                 RoutesImagensCotacao.CADASTRO,
               )),
           camposEsqueceuSenha(size),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
-            child: Container(
-                height: size.height * 0.05,
-                decoration: BoxDecoration(
-                    border: Border.all(color: AppCores.ROXOPRINCIPAL, width: 4),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-                  child: Center(
-                    child: TextButton(
-                      child: Text(
-                        'Enviar',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: AppCores.ROXOPRINCIPAL,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: () => showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: Text('Recuperação de senha enviada'),
-                          content: Text(
-                              'Foi enviado um email para você seguir as instruções e recuperar a sua senha.'),
-                          actions: <Widget>[
-                            Center(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: size.width * 0.05),
-                                child: Container(
-                                  height: size.height * 0.05,
-                                  width: size.width,
-                                  decoration: BoxDecoration(
-                                      border: Border.all(
-                                          color: AppCores.ROXOPRINCIPAL,
-                                          width: 4),
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(20))),
-                                  child: TextButton(
-                                      child: Text(
-                                        'OK',
-                                        style: TextStyle(
-                                            fontSize: 16,
-                                            color: AppCores.ROXOPRINCIPAL,
-                                            fontWeight: FontWeight.bold),
-                                      ),
-                                      onPressed: () =>
-                                          Navigator.pop(context, 'OK')),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )),
-          )
+          _botao(size)
         ],
       ),
+    );
+  }
+
+  Widget _botao(Size size) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: size.width * 0.1),
+      child: Container(
+          height: size.height * 0.05,
+          decoration: BoxDecoration(
+              border: Border.all(color: AppCores.ROXOPRINCIPAL, width: 4),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Center(
+              child: TextButton(
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: AppCores.ROXOPRINCIPAL,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: () => showDialog<String>(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: Text('Recuperação de senha enviada'),
+                    content: Padding(
+                      padding: EdgeInsets.only(
+                          bottom: size.height * 0.01, top: size.height * 0.01),
+                      child: Text(
+                          'Foi enviado um email para você seguir as instruções e recuperar a sua senha.'),
+                    ),
+                    actions: <Widget>[
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                            left: size.width * 0.05,
+                            right: size.width * 0.05,
+                            bottom: size.height * 0.03,
+                          ),
+                          child: Container(
+                            height: size.height * 0.05,
+                            width: size.width,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: AppCores.ROXOPRINCIPAL, width: 4),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20))),
+                            child: TextButton(
+                                child: Text(
+                                  'OK',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppCores.ROXOPRINCIPAL,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () => Navigator.pop(context, 'OK')),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
