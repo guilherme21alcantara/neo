c6bf45c8547d5f6bc343cc88d1be159df30f6fe6
arthur <87774958+Arthur-T-Tavares@users.noreply.github.com>
2021-09-27 13:42:31 -0300

    corrigido 'a' faltando na palavra placa

--- lib/Screen/texto_globais/textos_gerais/Cotacao/cadastro.dart
+++ lib/Screen/texto_globais/textos_gerais/Cotacao/cadastro.dart
@@ -1,13 +1,11 @@
-abstract class CADASTRO
-{
+abstract class CADASTRO {
   //Subtitulo
-  static const SUBTITULO =  'Vamos cotar o seu Seguro?';
+  static const SUBTITULO = 'Vamos cotar o seu Seguro?';
   //nomes campos
   static const NOME = 'Nome';
-  static const CEL =  'Celular com (DDD)';
+  static const CEL = 'Celular com (DDD)';
   static const Email = 'Email';
-  static const PLACA = 'Qual a plca do seu veículo?';
+  static const PLACA = 'Qual a placa do seu veículo?';
   //botao
   static const PROXIMO = 'Próximo';
-
 }
