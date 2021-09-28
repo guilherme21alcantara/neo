3a4923fd28c9840416dd845beaa73755a86b7a37
arthur <87774958+Arthur-T-Tavares@users.noreply.github.com>
2021-09-28 14:34:04 -0300

    slider

---
+++ lib/Screen/componentes_globais/slider_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:neo/Screen/componentes_globais/componentes_temas/cores.dart';
+
+class SliderBar extends StatefulWidget {
+  final double min;
+  final double max;
+  final Size size;
+  final int divisions;
+  const SliderBar(
+      {Key? key,
+      required this.min,
+      required this.max,
+      required this.size,
+      required this.divisions})
+      : super(key: key);
+
+  @override
+  _SliderBarState createState() => _SliderBarState();
+}
+
+class _SliderBarState extends State<SliderBar> {
+  late double _currentSliderValue = widget.min;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: widget.size.height * 0.01),
+      child: Container(
+        height: widget.size.height * 0.15,
+        width: widget.size.width,
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: widget.size.width * 0.75),
+              child: Text(
+                'Fipe',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Slider(
+              activeColor: AppCores.ROXOPRINCIPAL,
+              inactiveColor: AppCores.CINZA,
+              value: _currentSliderValue,
+              min: widget.min,
+              max: widget.max,
+              divisions: widget.divisions,
+              label: _currentSliderValue.round().toString(),
+              onChanged: (double value) {
+                setState(() {
+                  _currentSliderValue = value.toInt().toDouble();
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: widget.size.width * 0.75),
+              child: Text(
+                'R\$$_currentSliderValue',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Screen/conta/home.dart
+++ lib/Screen/conta/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neo/Screen/componentes_globais/home_header_button.dart';
 import 'package:neo/Screen/componentes_globais/scaffold_home.dart';
+import 'package:neo/Screen/componentes_globais/slider_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,7 +15,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     var size = mediaQuery.size;
-    return ScaffoldHome(
-        conteudo: [HomeHeaderButton(size: size)], title: 'Olá João');
+    return ScaffoldHome(conteudo: [
+      SliderBar(
+        size: size,
+        min: 0,
+        max: 5000,
+        divisions: 10,
+      )
+    ], title: 'Olá João');
   }
 }
